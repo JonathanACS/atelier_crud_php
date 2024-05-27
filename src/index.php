5fb9272e8bcbfa8aacc8c8335fb93bb9cc1cfec4
JonathanACS <jonathan.beze580@gmail.com>
2024-05-27 15:04:25 +0200

    tableau stage

--- src/index.php
+++ src/index.php
@@ -1,66 +1,38 @@
-<?php
-
-session_start();
-//PHP connect
-
-    require_once("connect.php");
-
-    $sql = "SELECT * FROM users";
-
-        //on prépare le requête 
-    $query = $db->prepare($sql);
-        //execution de la requete
-    $query->execute();
-        //recupération des données sous forme de tableau associatif
-    $users = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    // echo "<pre>";
-    // print_r($users);
-    // echo "</pre>";
-?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atelier CRUD PHP</title>
+    <title>Tableau Crud PHP</title>
 </head>
 <body>
-    <h1>Liste des utilisateurs</h1>
-    <?php 
-        if(!empty($_SESSION["message"])){
-            echo "<p>" . $_SESSION["message"] . "</p>";
-            $_SESSION["message"] = "";
-        }
-    ?>
+    <h1>Tableau crud stage</h1>
     <table>
         <thead>
-            <td>ID</td>
-            <td>Prénom</td>
-            <td>Nom</td>
-            <td>Actions</td>
+            <td>id</td>
+            <td>statut</td>
+            <td>entreprise</td>
+            <td>date</td>
+            <td>date_de_relance</td>
+            <td>type_de_postulation</td>
+            <td>website</td>
+            <td>intitulé_du_poste</td>
+            <td>type_de_contract</td>
+            <td>mail</td>
+            <td>commentaires</td>
         </thead>
         <tbody>
-            <?php 
-            // pour chaque utilisateur récupéré dans $users, on affiche une nouvelle ligne dans la table HTML
-                foreach($users as $user){
-                    //chaque utilisateur récupéré sera identifier en tant que $user dans le foreach
-                ?>
-                 <tr> 
-                    <td><?=$user["id"]?></td>
-                    <td><?=$user["first_name"]?></td>
-                    <td><?=$user["last_name"]?></td>
-                    <td>
-                        <a href="user.php?id=<?=$user["id"]?>">Voir</a>
-                        <a href="update.php?id=<?=$user["id"]?>">Mettre à jour</a>
-                        <a href="delete.php?id=<?=$user["id"]?>">Supprimer</a>
-                    </td>
-                 </tr>
-                <?php
-                }
-            ?>
-
-            <a href="form.php">Ajoutez un utilisateur</a>
+            <td>1</td>
+            <td>pris</td>
+            <td>souris</td>
+            <td>2024-05-01</td>
+            <td>2024-05-31</td>
+            <td>rose</td>
+            <td>recherche</td>
+            <td>développeur web</td>
+            <td>stage</td>
+            <td>123@mail.com</td>
+            <td>voici un commentaire</td>
         </tbody>
     </table>
 </body>
